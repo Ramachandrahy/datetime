4f8671bf6b7464d06966ef003d50b89c246ffc31
Ramachandrahy <hyramachandra@gmail.com>
2024-05-20 20:27:24 +0530

    datetime project has done

---
+++ coding1/project1/app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import datetime as dt
+def home(request):
+    time1=dt.datetime.now()
+    time2=time1+dt.timedelta(hours=4)
+    time3=time1+dt.timedelta(hours=-4)
+    return render(request,'app1/index.html',{'param1':time1,'param2':time2,'param3':time3})
